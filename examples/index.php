7a324066d2f3786cfee28c0f42d51cac06fbcac8
merlijnbruijnes <merlijnbruijnes@gmail.com>
2019-05-29 16:07:07 +0200

    example updates

--- examples/index.php
+++ examples/index.php
@@ -7,13 +7,17 @@ require_once '../src/MRBApiClient.php';
 
 $client = new MrbApiClient(
     [
-        'client_id'     => '',
-        'client_secret' => '',
-        'username'      => '',
-        'password'      => '',
+        'client_id'     => '5_5yxhp4owymsc0sgs4wsc4k4swo80oog0kks8okg0sgkssco8s4',
+        'client_secret' => '5v0llq4czcgsw8gw848k8s8sgcc04cocks8w08co4sgs8ww8cs',
+        'username'      => 'api',
+        'password'      => 'H6HaZB',
     ]
 );
 
-$testArray = $client->get('/web-shop/module/editor/site/document/all');
+$data = json_decode(
+    $client->get('/web-shop/module/products/site/products/all'),
+    true
+);
 
-print_r($testArray);
+print_r('<PRE>');
+print_r($data);
